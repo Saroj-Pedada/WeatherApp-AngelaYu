0500367b73df32bfbbc4332c81583d5887d2335c
Saroj Pedada <77237839+Saroj-Pedada@users.noreply.github.com>
2023-01-28 16:31:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get("/", function (req, res) {
 app.post("/", function (req, res) {
     const query = req.body.cityName;
     const unit = "metric";
-    const appKey = "45321788ba3e6e9e4494e70ba22b9f1d";
+    const appKey = APPKEY;
     const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + appKey + "&units=" + unit;
     https.get(url, function (response) {
         console.log(response.statusCode);
